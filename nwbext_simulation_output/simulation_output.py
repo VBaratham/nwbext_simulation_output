8e89b4615210ebeb6397cd94be8818822a430a1a
bendichter <ben.dichter@gmail.com>
2019-01-18 12:03:43 +0200

    automatically find namespace path

--- nwbext_simulation_output/simulation_output.py
+++ nwbext_simulation_output/simulation_output.py
@@ -1,4 +1,7 @@
+import os
 from pynwb import load_namespaces, get_class
 
-load_namespaces('simulation_output.namespace.yaml')
+namespace_path = os.path.join(os.path.split(__file__)[0], 'simulation_output.namespace.yaml')
+
+load_namespaces(namespace_path)
 CompartmentSeries = get_class('CompartmentSeries', 'simulation_output')
